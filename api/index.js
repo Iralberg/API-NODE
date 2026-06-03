2cd6ade1f01aa364c9ba467caaf2eca79a7aab80
iralberg <iralbergsampaio@gmail.com>
2026-06-03 08:23:05 -0300

    update 1.3

--- api/index.js
+++ api/index.js
@@ -6,58 +6,44 @@ import mongoose from "mongoose";
 import dns from "dns";
 import cors from "cors";
 
-
-
-
-
-
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
-const app = express();
-app.use(cors());
 
+const app = express();
+
+app.use(cors());
 app.use(express.json());
 
-
 const MONGO_URI = process.env.MONGO_URI;
-const PORT = process.env.PORT || 3000;
-
-
 
 const usuarioSchema = new mongoose.Schema({
   nome: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   idade: { type: Number, required: true }
-}, { timestamps: true })
+}, { timestamps: true });
 
-const Usuario = mongoose.model("Usuario", usuarioSchema);
+const Usuario = mongoose.models.Usuario || mongoose.model("Usuario", usuarioSchema);
 
+let isConnected = false;
 
-//criar usuarios
+async function connectDB() {
+  if (isConnected) return;
+
+  await mongoose.connect(MONGO_URI);
+  isConnected = true;
+}
+
 app.post("/usuarios", async (req, res) => {
+  await connectDB();
+
   const usuariocriado = await Usuario.create(req.body);
   res.status(201).json(usuariocriado);
 });
 
-//Listar usuarios
 app.get("/usuarios", async (req, res) => {
+  await connectDB();
+
   const usuarios = await Usuario.find();
   res.status(200).json(usuarios);
 });
 
-
-
-async function startServer() {
-  try {
-    await mongoose.connect(MONGO_URI);
-    console.log("✅ Conectado ao MongoDB Atlas");
-
-    app.listen(PORT, () => {
-      console.log(`🚀 Servidor rodando na porta ${PORT}`);
-    });
-
-  } catch (error) {
-    console.error("❌ Erro ao conectar:", error);
-  }
-}
-
-startServer();
+export default app;
